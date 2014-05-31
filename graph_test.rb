ef5c89c2b36734b5aff928edd46c8a10266a67bc
Manan Shah <manan.shah.777@gmail.com>
2014-05-31 01:15:03 -0700

    Improve graph_test for dfs and bfs

--- graph_test.rb
+++ graph_test.rb
@@ -2,6 +2,7 @@ require_relative 'graph'
 require_relative 'node'
 require_relative 'edge'
 require_relative 'dfs'
+require_relative 'bfs'
 
 g = Graph.new
 
@@ -12,13 +13,26 @@ g.add_node(n)
 puts g.degree(n)
 x = Node.new("b")
 g.add_node(x)
-g.connect(x, n)
-puts g.degree(x)
-puts g.degree(n)
+y = Node.new("c")
+g.add_node(y)
 
-dfs = DFS.new(g,n)
+puts "\n"       
+
+g.add_edge(x, y)
+g.add_edge(y, n)
+
+dfs = DFS.new(g,x)
 t = dfs.path_to(n)
 
+bfs = BFS.new(g,x)
+t2 = bfs.path_to(n)
+
+puts "BFS: \n"
 t.each do |w|
+ puts w.to_str
+end
+
+puts "DFS: \n"
+t2.each do |w|
  puts w.to_str
 end
