544d447720bb7fa6e105d946f133d48e1df70cc5
Manan Shah <manan.shah.777@gmail.com>
2014-05-30 20:09:20 -0700

    Update edge.rb

--- edge.rb
+++ edge.rb
@@ -1,8 +1,12 @@
 class Edge
-  attr_accessor :from, :to, :weight
+  attr_accessor :from
+  attr_accessor :to
+  attr_accessor :weight
 
   def initialize(from, to, weight)
-    @from, @to, @weight = from, to, weight
+    @from = from
+    @to = to
+    @weight = weight
   end
 
   def <=>(other)
