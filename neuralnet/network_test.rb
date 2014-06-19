c5966febaac9bbd24d931d2d66cc141dadf52e88
Manan Shah <manan.shah.777@gmail.com>
2014-06-19 16:06:12 -0700

    Add test for NeuralNetwork.rb

---
+++ neuralnet/network_test.rb
@@ -0,0 +1,19 @@
+require_relative "NeuralNetwork.rb"
+
+learning_rate = 10 #|rate|
+momentum_rate = 1 #|momentum|
+
+neural_net = NeuralNetwork.new(:inputs => 2, :hidden_nodes => 10, :output_nodes => 1)
+
+10000.times do |i|
+     neural_net.train([0, 0], [0], learning_rate, momentum_rate)
+     neural_net.train([0, 1], [0], learning_rate, momentum_rate)
+     neural_net.train([1, 0], [0], learning_rate, momentum_rate)
+     neural_net.train([1, 1], [1], learning_rate, momentum_rate)
+end
+
+puts "[0, 0] -> " + neural_net.predict([0, 0]).to_s
+puts "[1, 0] -> " + neural_net.predict([1, 0]).to_s
+puts "[0, 1] -> " + neural_net.predict([0, 1]).to_s
+puts "[1, 1] -> " + neural_net.predict([1, 1]).to_s
+puts "done." 
